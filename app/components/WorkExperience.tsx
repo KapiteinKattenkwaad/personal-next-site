571ed0d0f93dc3802f8e8008877edc9403784a84
KapiteinKattenkwaad <maxstouten@hotmail.com>
2025-06-26 10:12:31 +1000

    added social icons and css changes

--- app/components/WorkExperience.tsx
+++ app/components/WorkExperience.tsx
@@ -1,15 +1,18 @@
 'use client';
 import { motion } from 'framer-motion';
+import { a, span } from 'framer-motion/client';
 
 const experiences = [
     {
         company: 'BugHerd',
+        link: 'https://bugherd.com/',
         role: 'Software Developer',
         period: 'Feb 2023 – May 2025',
         stack: 'React (TypeScript), Ruby on Rails, Node.js',
     },
     {
         company: 'Icon Agency',
+        link: 'https://iconagency.com.au/',
         role: 'Front‑end Developer',
         period: 'Jan 2022 – Feb 2023',
         stack: 'Vue 3, Next.js, SCSS, JavaScript, WordPress',
@@ -52,7 +55,21 @@ export default function WorkExperience() {
                         transition={{ delay: i * 0.1, duration: 0.6 }}
                         className="border-l-2 border-teal-500 pl-6 relative"
                     >
-                        <h3 className="text-xl font-semibold">{exp.role} @ {exp.company}</h3>
+                        <h3 className="text-xl font-semibold">
+                            {exp.role} &nbsp;
+                           
+                            {
+                                exp.link ? (
+                                    <a className='relative text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 via-blue-500 to-purple-600 after:content-[""] after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-gradient-to-r after:from-cyan-400 after:via-blue-500 after:to-purple-600' href={exp.link} target='_blank'>
+                                        @ {exp.company}
+                                    </a>
+                                )
+                                    : <span>
+                                       @  {exp.company}
+                                    </span>
+                            }
+
+                        </h3>
                         <p className="text-sm text-neutral-400 mb-1">{exp.period}</p>
                         <p className="text-sm text-neutral-300">Stack: {exp.stack}</p>
                     </motion.div>
